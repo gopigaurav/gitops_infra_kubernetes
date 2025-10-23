048a142c89366309955d5b3c7287ae657fe3f3e2
gopigaurav <gopigaurav9@gmail.com>
2025-10-23 15:53:01 +0530

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,24 @@
 #   }
 # }
 
-# update code for kind-config.yaml
+# When we are trying to create the new cluster
 resource "null_resource" "kind_cluster" {
   provisioner "local-exec" {
     command = "kind create cluster --name dev-cluster --config=${path.module}/kind-config.yaml"
+  }
+}
+
+
+resource "null_resource" "wait_for_cluster" {
+  depends_on = [null_resource.kind_cluster]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for kind cluster dev-cluster to be ready..."
+      for i in {1..30}; do
+        kubectl get nodes && break || sleep 2
+      done
+    EOT
   }
 }
 
@@ -27,6 +41,7 @@ resource "helm_release" "monitoring" {
   depends_on = [
     null_resource.kind_cluster,
     null_resource.helm_repo_update,
+    null_resource.wait_for_cluster
   ]
 
   name       = "monitoring"
@@ -53,7 +68,7 @@ resource "helm_release" "kafka" {
   # Give Helm/cluster more time to pull images and start the Kafka pods
   timeout = 600
   force_update = true 
-  depends_on = [null_resource.kind_cluster]
+  depends_on = [null_resource.kind_cluster, null_resource.wait_for_cluster]
 }
 
 # Producer
@@ -64,7 +79,7 @@ resource "helm_release" "producer" {
   values = [
     file("${path.module}/../overlays/dev/producer-values.yaml")
   ]
-  depends_on = [helm_release.kafka]
+  depends_on = [helm_release.kafka, null_resource.wait_for_cluster]
 }
 
 # Consumer
@@ -75,7 +90,7 @@ resource "helm_release" "consumer" {
   values = [
     file("${path.module}/../overlays/dev/consumer-values.yaml")
   ]
-  depends_on = [helm_release.kafka]
+  depends_on = [helm_release.kafka, null_resource.wait_for_cluster]
 }
 
 # Optional: ArgoCD
@@ -85,10 +100,11 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   namespace  = "argocd"
   create_namespace = true
+  timeout = 600
   values = [
     file("${path.module}/../charts/monitoring/argocd-values.yaml")
   ]
-  depends_on = [null_resource.kind_cluster]
+  depends_on = [null_resource.kind_cluster, null_resource.wait_for_cluster]
 }
 
 # Optional: Jenkins
@@ -98,24 +114,25 @@ resource "helm_release" "jenkins" {
   chart      = "jenkins"
   namespace  = "jenkins"
   create_namespace = true
+  timeout = 600
   values = [
     file("${path.module}/../charts/monitoring/jenkins-values.yaml")
   ]
   # values           = [file("${path.module}/../overlays/dev/jenkins-values.yaml")]
-  depends_on = [null_resource.kind_cluster]
+  depends_on = [null_resource.kind_cluster, null_resource.kind_cluster, null_resource.wait_for_cluster]
 }
 
 
-##############################
+#############################
 # Kubernetes Dashboard Helm Release
-##############################
+#############################
 
 
 ##############################
 # 1. Helm Release: Kubernetes Dashboard
 ##############################
 resource "helm_release" "kubernetes_dashboard" {
-  depends_on      = [null_resource.helm_repo_update]
+  depends_on      = [null_resource.helm_repo_update, null_resource.wait_for_cluster]
 
   name             = "kubernetes-dashboard"
   chart            = "kubernetes-dashboard"
@@ -144,7 +161,7 @@ resource "kubernetes_service_account" "dashboard_admin" {
     namespace = "kubernetes-dashboard"
   }
 
-  depends_on = [helm_release.kubernetes_dashboard]
+  depends_on = [helm_release.kubernetes_dashboard, null_resource.wait_for_cluster]
 }
 
 ##############################
@@ -167,7 +184,7 @@ resource "kubernetes_cluster_role_binding" "dashboard_admin" {
     namespace = kubernetes_service_account.dashboard_admin.metadata[0].namespace
   }
 
-  depends_on = [kubernetes_service_account.dashboard_admin]
+  depends_on = [kubernetes_service_account.dashboard_admin, null_resource.wait_for_cluster]
 }
 
 ##############################
@@ -221,5 +238,52 @@ output "dashboard_token_instructions" {
 
 
 # Roles related
-#kubectl auth can-i list pods --as=system:serviceaccount:kubernetes-dashboard:dashboard-admin
-#kubectl auth can-i get deployments --all-namespaces --as=system:serviceaccount:kubernetes-dashboard:dashboard-admin
+# kubectl auth can-i list pods --as=system:serviceaccount:kubernetes-dashboard:dashboard-admin
+# kubectl auth can-i get deployments --all-namespaces --as=system:serviceaccount:kubernetes-dashboard:dashboard-admin
+
+
+
+# port forwarding for argocd server 
+# kubectl port-forward svc/argocd-server -n argocd 8081:80 => username - admin
+# application - https://localhost:8081/applications
+
+
+# TO get the argocd password - windows
+# [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}")))
+
+# alternate command to get the password from argocd
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
+
+
+# TO get the jenkins username - windows
+# $Username = kubectl get secret jenkins -n jenkins -o jsonpath="{.data.jenkins-admin-user}" [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Username))
+
+
+# To get the jenkins password - windows
+# $Password = kubectl get secret jenkins -n jenkins -o jsonpath="{.data.jenkins-admin-password}" [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Password))
+# [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secret jenkins -n jenkins -o jsonpath="{.data.jenkins-admin-password}")))
+
+
+# Alternate to get jenkins password
+# kubectl get secret jenkins-admin -n jenkins -o jsonpath="{.data.password}" | base64 --decode
+# kubectl get secret jenkins-admin -n jenkins -o jsonpath="{.data.username}" | base64 --decode
+
+
+# To get the token from kubernetes dashboard
+# kubectl -n kubernetes-dashboard create token dashboard-admin
+
+
+# To get the jenkins service info
+# kubectl get svc jenkins -n jenkins
+
+
+# Uing kaniko for jenkins docker
+# kubectl create secret generic dockerhub-creds \
+#  --from-file=config.json=/path/to/docker/config.json \
+#  -n jenkins
+
+
+
+# To create config.json for jenkinsfile to use kaniko
+# kubectl create secret generic dockerhub-creds --from-file=config.json=C:\Users\gopig\.docker\config.json -n jenkins 
+# And to check whether its created or not - kubectl get secrets -n jenkins
